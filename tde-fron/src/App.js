75895edf335425e2a72627dca6fe856357cf9a89
Leonardo Pedroso <116748167+PedrosoleoSilva@users.noreply.github.com>
2023-06-13 20:46:42 -0300

    13-06

--- tde-fron/src/App.js
+++ tde-fron/src/App.js
@@ -1,17 +1,36 @@
-
+import { useState } from 'react';
 import'./components/Header.css';
-import Header from './components/Header';
+import UserInfo from './components/UserInfo';
+
+const list = ['Teste nome',"10/10/10","teste@teste.com"];
+
+const list1 = [
+  {
+    name: 'Teste nome',
+    birthday: '10/10/10',
+    email: "teste@teste.com"
+},
+{
+    name: 'Teste nome 2',
+    birthday: '10/10/10',
+    email: "teste2@teste.com"
+},
+{
+    name: 'Teste nome 3',
+    birthday: '10/10/10',
+    email: "teste3@teste.com"
+},
+];
 
 function App() {
   return (
     <div className="App">
       <div className="App-header">
-       <Header/>
-        <div className='texto'>
-          <h3>HEADER</h3>
+      </div> 
+        <div className='print'>
+          <UserInfo items={list1}/>
         </div>
       </div>
-    </div>
   );
 }
 
